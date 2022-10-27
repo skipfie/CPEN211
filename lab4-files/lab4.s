ed2a02415900b90a118dfcf33a36cb5018f6a9cf
Joe <skipfie@gmail.com>
2022-10-27 14:05:02 -0700

    start the pain

--- lab4-files/lab4.s
+++ lab4-files/lab4.s
@@ -1,7 +1,8 @@
 .globl binary_search
 binary_search:
-  mov pc,lr 
-
-
-
-  STR r0, []
+// int *numbers: r0 , int key: r1 , int length: r2
+  mov r3,#0 // int startIndex = 0
+  sub r4,r2,#1 //
+  
+  str r0, []
+  mov pc,lr
