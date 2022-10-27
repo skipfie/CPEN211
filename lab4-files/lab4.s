7d0ad5139e32e3e19e44cc906e129a8bb1a6fbdf
Joe <skipfie@gmail.com>
2022-10-27 15:47:37 -0700

    yuor mom 6

--- lab4-files/lab4.s
+++ lab4-files/lab4.s
@@ -16,22 +16,22 @@ L1:     cmn r6,#1 // while (keyIndex == -1)
         cmp r3,r4 // if (startIndex > endIndex)
         bhi Break
         
-        ldr r8,[r0,r5] // x = = numbers [ middleIndex ]
+        ldr r8,[r0,r5,LSL#2] // x = = numbers [ middleIndex ]
         cmp r8,r1 // else if (numbers [ middleIndex ] == key)
         moveq r6,r5 // keyIndex = middleIndex
 
-        subgt r4,r5,1 // else if (numbers [ middleIndex ] > key); endIndex = middleIndex - 1
+        subgt r4,r5,#1 // else if (numbers [ middleIndex ] > key); endIndex = middleIndex - 1
 
-        addlt r3,r5,1 // else; startIndex = middleIndex + 1
+        addlt r3,r5,#1 // else; startIndex = middleIndex + 1
 
-        str r7,[r0,r5] // numbers[ middleIndex ] = NumIters (we switched it to negative)
+        str r7,[r0,r5,LSL#2] // numbers[ middleIndex ] = NumIters (we switched it to negative)
 
         sub r8,r4,r3 // x = endIndex - startIndex
         lsr r9,r8,#1 // y = x/2
 
         add r5,r3,r9 // middleIndex = startIndex + y
 
-        sub r7,1 // NumIters--
+        sub r7,r7,#1 // NumIters--
 
         b L1 // while keyIndex == -1
 Break:
