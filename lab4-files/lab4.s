7a4e01387222b4d9bb1aefe90e7d9cb13539a998
Joe <skipfie@gmail.com>
2022-10-27 14:17:16 -0700

    pai n

--- lab4-files/lab4.s
+++ lab4-files/lab4.s
@@ -1,8 +1,11 @@
 .globl binary_search
 binary_search:
 // int *numbers: r0 , int key: r1 , int length: r2
-  mov r3,#0 // int startIndex = 0
-  sub r4,r2,#1 //
-  
+  mov r3,#0         // int startIndex = 0
+  sub r4,r2,#1      // int endIndex = length - 1
+  lsr r5,r4,#1      // int middleIndex = endIndex/2
+  mov r6,#-1
+
+
   str r0, []
   mov pc,lr
