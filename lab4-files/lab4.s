f711ed71b86e06d8b74ec483fc5b7dc1dfa9078f
Joe <skipfie@gmail.com>
2022-10-27 15:31:04 -0700

    your mom 4

--- lab4-files/lab4.s
+++ lab4-files/lab4.s
@@ -5,8 +5,9 @@ binary_search:
         sub r4,r2,#1        // int endIndex = length - 1
         lsr r5,r4,#1        // int middleIndex = endIndex/2
         mov r6,#-1          // int keyIndex = -1
-        mov r7,#1           // int NumIters = 1
+        mov r7,#-1          // int NumIters = -1
         mov r8,#0           // int x
+        mov r9,#0           // int y
         
 
 L1:     cmn r6,#1 // while (keyIndex == -1)
@@ -19,13 +20,20 @@ L1:     cmn r6,#1 // while (keyIndex == -1)
         cmp r8,r1 // else if (numbers [ middleIndex ] == key)
         moveq r6,r5 // keyIndex = middleIndex
 
-        subhi r6,r5,1 // else if (numbers [ middleIndex ] > key); keyIndex = middleIndex - 1
+        subgt r4,r5,1 // else if (numbers [ middleIndex ] > key); endIndex = middleIndex - 1
 
-        addlo r6,r5,1 // else; keyIndex = middleIndex + 1
+        addlt r3,r5,1 // else; startIndex = middleIndex + 1
 
-        
-        
+        str r7,[r0,r5] // numbers[ middleIndex ] = NumIters (we switched it to negative)
+
+        sub r8,r4,r3 // x = endIndex - startIndex
+        lsr r9,r8,#1 // y = x/2
+
+        add r5,r3,r9 // middleIndex = startIndex + y
+
+        sub r7,1 // NumIters--
+
         b L1 // while keyIndex == -1
 Break:
-        str r0, []
+        mov r0,r6
         mov pc,lr
