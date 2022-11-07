626e062d43329d65c45a20fa2e709524305672fe
Joe <skipfie@gmail.com>
2022-11-07 15:53:40 -0800

    alu your mom

--- lab5/lab5-files/alu.sv
+++ lab5/lab5-files/alu.sv
@@ -1,3 +1,21 @@
 module ALU(input [15:0] val_A, input [15:0] val_B, input [1:0] ALU_op, output [15:0] ALU_out, output Z);
-  // your implementation here
+    reg Z;
+    reg [15:0] nout;
+
+    always_comb begin
+        if (nout == 16'd0) begin
+            Z = 1'b1;
+        end else begin
+            Z = 0'b1;
+        end
+    end
+
+    always_comb begin
+        case (ALU_op) 
+        2'b00: nout = val_A + val_B;
+        2'b01: nout = val_A - val_B;
+        2'b10: nout = val_A & val_B;
+        2'b11: nout = ~val_B;
+        endcase
+    end
 endmodule: ALU
