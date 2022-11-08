8f81d30e7399686ea51f41527d5a8dd918ebdf89
Joe <skipfie@gmail.com>
2022-11-07 17:47:15 -0800

    git

--- lab5/lab5-files/shifter.sv
+++ lab5/lab5-files/shifter.sv
@@ -7,7 +7,7 @@ module shifter(input [15:0] shift_in, input [1:0] shift_op, output reg [15:0] sh
             2'b00: nout = shift_in;
             2'b01: nout = shift_in << 1;
             2'b10: nout = shift_in >> 1;
-            2'b11: nout = shift_in >>> 1;
+            2'b11: nout = $signed(shift_in) >>> 1;
         endcase
     end
 endmodule: shifter
--- lab5/lab5-files/tb_shifter.sv
+++ lab5/lab5-files/tb_shifter.sv
@@ -12,57 +12,63 @@ module tb_shifter(output err);
     initial begin
         shift_in = 16'b1010010001010101;
         shift_op = 2'b00;
+        #10;
         assert (shift_out === 16'b1010010001010101) $display("[PASS] B unchanged operation 00");
         else begin
             $error("[FAIL] B unchanged operation 00");
             nerr = 1'b1;
         end
-        #10;
+        
 
         shift_in = 16'b1010010001010101;
         shift_op = 2'b01;
+        #10;
         assert (shift_out === 16'b0100100010101010) $display("[PASS] B left shift one bit operation 01");
         else begin
             $error("[FAIL] B left shift one bit operation 01");
             nerr = 1'b1;
         end
-        #10;
+        
 
         shift_in = 16'b1010010001010101;
         shift_op = 2'b10;
+        #10;
         assert (shift_out === 16'b0101001000101010) $display("[PASS] B logical right shift one bit where MSB was 1, operation 10");
         else begin
             $error("[FAIL] B logical right shift one bit where MSB was 1, operation 10");
             nerr = 1'b1;
         end
-        #10;
+        
 
         shift_in = 16'b0010010001010101;
         shift_op = 2'b10;
+        #10;
         assert (shift_out === 16'b0001001000101010) $display("[PASS] B logical right shift one bit where MSB was 0, operation 10");
         else begin
             $error("[FAIL] B logical right shift one bit where MSB was 0, operation 10");
             nerr = 1'b1;
         end
-        #10;
+        
 
         shift_in = 16'b1010010001010101;
         shift_op = 2'b11;
+        #10;
         assert (shift_out === 16'b1101001000101010) $display("[PASS] B arithmetic right shift one bit where MSB was 1, operation 11");
         else begin
             $error("[FAIL] B arithmetic right shift one bit where MSB was 1, operation 11");
             nerr = 1'b1;
         end
-        #10;
+        
 
         shift_in = 16'b0010010001010101;
         shift_op = 2'b11;
+        #10;
         assert (shift_out === 16'b0001001000101010) $display("[PASS] B arithmetic right shift one bit where MSB was 0, operation 11");
         else begin
             $error("[FAIL] B arithmetic right shift one bit where MSB was 0, operation 11");
             nerr = 1'b1;
         end
-        #10;
+        
 
         $stop;
     end
