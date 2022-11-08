a86d9e4fbc6143062d4a1efc17507fb384b48a9e
Joe <skipfie@gmail.com>
2022-11-07 16:34:07 -0800

    e2

--- lab5/lab5-files/shifter.sv
+++ lab5/lab5-files/shifter.sv
@@ -1,3 +1,13 @@
 module shifter(input [15:0] shift_in, input [1:0] shift_op, output reg [15:0] shift_out);
-  // your implementation here
+    reg nout;
+    assign shift_out = nout;
+
+    always_comb begin
+        case (shift_op) 
+            2'b00: nout = ;
+            2'b01: nout = ;
+            2'b10: nout = ;
+            2'b11: nout = ;
+        endcase
+    end
 endmodule: shifter
