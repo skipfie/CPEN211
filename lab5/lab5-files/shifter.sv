e2576a6f8daa9b81797f196480591fd6ca9694ce
Joe <skipfie@gmail.com>
2022-11-07 16:41:26 -0800

    shifter done your mom

--- lab5/lab5-files/shifter.sv
+++ lab5/lab5-files/shifter.sv
@@ -4,10 +4,10 @@ module shifter(input [15:0] shift_in, input [1:0] shift_op, output reg [15:0] sh
 
     always_comb begin
         case (shift_op) 
-            2'b00: nout = ;
-            2'b01: nout = ;
-            2'b10: nout = ;
-            2'b11: nout = ;
+            2'b00: nout = shift_in;
+            2'b01: nout = shift_in << 1;
+            2'b10: nout = shift_in >> 1;
+            2'b11: nout = shift_in >>> 1;
         endcase
     end
 endmodule: shifter
