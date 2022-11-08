64732223b7e358ec5c901c63c9c7eb46fb2f9dbe
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-07 16:34:00 -0800

    e

--- lab5/lab5-files/tb_alu.sv
+++ lab5/lab5-files/tb_alu.sv
@@ -2,7 +2,7 @@ module tb_ALU(output err);
   reg [15:0] val_A;
   reg [15:0] val_B;
   reg [1:0] ALU_op;
-  reg nerr;
+  reg nerr = 1'b0;
   assign err = nerr;
   wire [15:0] ALU_out;
   wire Z;
@@ -16,10 +16,10 @@ module tb_ALU(output err);
     val_A = 16'd1; //A = 1
     val_B = 16'd1; //B = 1
     #5;
-    assert(ALU_out === 16'd2) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'd2) $display("[PASS] %d + %d = %d", val_A, val_B, ALU_out); 
     else begin
-      $error("[FAIL] incorrect output");
-      nerr = 1;
+      $error("[FAIL] %d + %d = %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
@@ -30,85 +30,85 @@ module tb_ALU(output err);
     val_A = 16'd69; //A = 69
     val_B = 16'd69; //B = 69
     #5;
-    assert(ALU_out === 16'd138) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'd138) $display("[PASS] %d + %d = %d correct output", val_A, val_B, ALU_out); 
     else begin
       $error("[FAIL] incorrect output");
-      nerr = 1;
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
       $error("[FAIL] incorrect Z");
-      nerr = 1;
+      nerr = 1'b1;
     end
 
     val_A = 16'd6969; //A = 6969
     val_B = 16'd6969; //B = 6969
     #5;
-    assert(ALU_out === 16'd13938) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'd13938) $display("[PASS] %d + %d = %d correct output", val_A, val_B, ALU_out); 
     else begin
       $error("[FAIL] incorrect output");
-      nerr = 1;
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
       $error("[FAIL] incorrect Z");
-      nerr = 1;
+      nerr = 1'b1;
     end
 
     val_A = 16'd29; //A = 29
     val_B = 16'd284; //B = 284
     #5;
-    assert(ALU_out === 16'd313) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'd313) $display("[PASS] %d + %d = %d correct output", val_A, val_B, ALU_out); 
     else begin
       $error("[FAIL] incorrect output");
-      nerr = 1;
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
       $error("[FAIL] incorrect Z");
-      nerr = 1;
+      nerr = 1'b1;
     end
 
     val_A = 16'd1; //A = -1
     val_B = 16'd1; //B = -1
     #5;
-    assert(ALU_out === 16'b) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'b) $display("[PASS] %d + %d = %d correct output", val_A, val_B, ALU_out); 
     else begin
       $error("[FAIL] incorrect output");
-      nerr = 1;
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
       $error("[FAIL] incorrect Z");
-      nerr = 1;
+      nerr = 1'b1;
     end
 
     val_A = 16'd1; //A = 1
     val_B = 16'd1; //B = 1
     #5;
-    assert(ALU_out === 16'd2) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'd2) $display("[PASS] %d + %d = %d correct output", val_A, val_B, ALU_out); 
     else begin
       $error("[FAIL] incorrect output");
-      nerr = 1;
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
       $error("[FAIL] incorrect Z");
-      nerr = 1;
+      nerr = 1'b1;
     end
 
     val_A = 16'd1; //A = 1
     val_B = 16'd1; //B = 1
     #5;
-    assert(ALU_out === 16'd2) $display("[PASS] correct output"); 
+    assert(ALU_out === 16'd2) $display("[PASS] %d + %d = %d correct output", val_A, val_B, ALU_out); 
     else begin
       $error("[FAIL] incorrect output");
-      nerr = 1;
+      nerr = 1'b1;
     end
     assert(Z === 1'b0) $display("[PASS] Z = 0"); 
     else begin
       $error("[FAIL] incorrect Z");
-      nerr = 1;
+      nerr = 1'b1;
     end
 
     //testing subtraction
