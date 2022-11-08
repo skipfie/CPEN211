cde8cbd1656ce1787b19e84b0cd16a56438939fa
Joe <skipfie@gmail.com>
2022-11-07 16:02:13 -0800

    nZ

--- lab5/lab5-files/alu.sv
+++ lab5/lab5-files/alu.sv
@@ -1,12 +1,12 @@
 module ALU(input [15:0] val_A, input [15:0] val_B, input [1:0] ALU_op, output [15:0] ALU_out, output Z);
-    reg Z;
+    reg nZ;
     reg [15:0] nout;
 
     always_comb begin
         if (nout == 16'd0) begin
-            Z = 1'b1;
+            nZ = 1'b1;
         end else begin
-            Z = 0'b1;
+            nZ = 0'b1;
         end
     end
 
