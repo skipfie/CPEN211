de6d502870a2357a0d47918538ad93043610c479
Joe <skipfie@gmail.com>
2022-11-08 17:23:27 -0800

    deliverables

---
+++ lab5/lab5-deliverables/tb_alu.sv
@@ -0,0 +1,503 @@
+module tb_ALU(output err);
+  reg nerr = 1'b0;
+  assign err = nerr;
+
+  reg [1:0] ALU_op;
+  reg signed [15:0] val_A, val_B, ALU_out;
+  wire Z;
+
+  ALU dut(.val_A(val_A), .val_B(val_B), .ALU_op(ALU_op), .ALU_out(ALU_out), .Z(Z));
+
+  initial begin
+
+    $display("testing addition & output isn't 0");
+    ALU_op = 2'b00;
+    val_A = 16'd1; //A = 1
+    val_B = 16'd1; //B = 1
+    #5;
+    assert(ALU_out === 16'd2) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+    
+    val_A = 16'd69; //A = 69
+    val_B = 16'd69; //B = 69
+    #5;
+    assert(ALU_out === 16'd138) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd6969; //A = 6969
+    val_B = 16'd6969; //B = 6969
+    #5;
+    assert(ALU_out === 16'd13938) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd29; //A = 29
+    val_B = 16'd284; //B = 284
+    #5;
+    assert(ALU_out === 16'd313) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd1; //A = -1
+    val_B = -16'd1; //B = -1
+    #5;
+    assert(ALU_out === -16'd2) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd69; //A = -69
+    val_B = -16'd69; //B = -69
+    #5;
+    assert(ALU_out === -16'd138) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd4097; //A = -4097
+    val_B = -16'd2847; //B = -2847
+    #5;
+    assert(ALU_out === -16'd6944) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+
+    $display("testing addition & output is 0");
+    val_A = 16'd0; //A = 0
+    val_B = 16'd0; //B = 0
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd259; //A = -259
+    val_B = 16'd259; //B = 259
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd3749; //A = 3749
+    val_B = -16'd3749; //B = -3749
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d + %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d + %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+
+    $display("testing subtraction & output isn't 0");
+    ALU_op = 2'b01;
+    val_A = 16'd2; //A = 2
+    val_B = 16'd1; //B = 1
+    #5;
+    assert(ALU_out === 16'd1) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd70; //A = 70
+    val_B = 16'd1; //B = 1
+    #5;
+    assert(ALU_out === 16'd69) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd13812; //A = 13812
+    val_B = 16'd6843; //B = 6843
+    #5;
+    assert(ALU_out === 16'd6969) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd6808; //A = 6808
+    val_B = 16'd5739; //B = 5739
+    #5;
+    assert(ALU_out === 16'd1069) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd1; //A = 1
+    val_B = 16'd2; //B = 2
+    #5;
+    assert(ALU_out === -16'd1) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd1; //A = 1
+    val_B = 16'd70; //B = 70
+    #5;
+    assert(ALU_out === -16'd69) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd1; //A = -1
+    val_B = 16'd7000; //B = 7000
+    #5;
+    assert(ALU_out === -16'd7001) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    $display("testing subtraction & output is 0");
+    val_A = 16'd0; //A = 0
+    val_B = 16'd0; //B = 0
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+    
+    val_A = 16'd69; //A = 69
+    val_B = 16'd69; //B = 69
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'd4369; //A = 4369
+    val_B = 16'd4369; //B = 4369
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd43; //A = 43
+    val_B = -16'd43; //B = 43
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = -16'd1969; //A = 1969
+    val_B = -16'd1969; //B = 1969
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %d - %d > %d | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %d - %d > %d", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+
+    $display("testing AND & output isn't 0"); 
+    ALU_op = 2'b10;
+    val_A = 16'b1111111111111111; 
+    val_B = 16'b0000000000000001; 
+    #5;
+    assert(ALU_out === 16'b0000000000000001) $display("[PASS] %b & %b > %b | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %b & %b > %b", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'b1111111111111111; 
+    val_B = 16'b0100000000000001; 
+    #5;
+    assert(ALU_out === 16'b0100000000000001) $display("[PASS] %b & %b > %b | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %b & %b > %b", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'b1111111111111111; 
+    val_B = 16'b1110000000000001; 
+    #5;
+    assert(ALU_out === 16'b1110000000000001) $display("[PASS] %b & %b > %b | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %b & %b > %b", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'b1111110111111111; 
+    val_B = 16'b0000111100000001; 
+    #5;
+    assert(ALU_out === 16'b0000110100000001) $display("[PASS] %b & %b > %b | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %b & %b > %b", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+  
+    $display("testing AND & output is 0");
+    val_A = 16'b0000000000000000; 
+    val_B = 16'b0000000000000000; 
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %b & %b > %b | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %b & %b > %b", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_A = 16'b1111111111111111; 
+    val_B = 16'b0000000000000000; 
+    #5;
+    assert(ALU_out === 16'd0) $display("[PASS] %b & %b > %b | correct output", val_A, val_B, ALU_out); 
+    else begin
+      $error("[FAIL] %b & %b > %b", val_A, val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+
+    $display("testing negation & output isn't 0");
+    ALU_op = 2'b11;
+    val_B = 16'b0000000000000001; 
+    #5;
+    assert(ALU_out === 16'b1111111111111110) $display("[PASS] ~%b > %b | correct output", val_B, ALU_out); 
+    else begin
+      $error("[FAIL] ~%b > %b", val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_B = 16'b0100000000000001; 
+    #5;
+    assert(ALU_out === 16'b1011111111111110) $display("[PASS] ~%b > %b | correct output", val_B, ALU_out); 
+    else begin
+      $error("[FAIL] ~%b > %b", val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_B = 16'b1111000000000001; 
+    #5;
+    assert(ALU_out === 16'b0000111111111110) $display("[PASS] ~%b > %b | correct output", val_B, ALU_out); 
+    else begin
+      $error("[FAIL] ~%b > %b", val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin  
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_B = 16'b0000000001100001; 
+    #5;
+    assert(ALU_out === 16'b1111111110011110) $display("[PASS] ~%b > %b | correct output", val_B, ALU_out); 
+    else begin
+      $error("[FAIL] ~%b > %b", val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    val_B = 16'b0001100001100001; 
+    #5;
+    assert(ALU_out === 16'b1110011110011110) $display("[PASS] ~%b > %b | correct output", val_B, ALU_out); 
+    else begin
+      $error("[FAIL] ~%b > %b", val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b0) $display("[PASS] Z = 0 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 1 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+
+    $display("testing negation & output is 0");
+    val_B = 16'b1111111111111111; 
+    #5; 
+    assert(ALU_out === 16'd0) $display("[PASS] ~%b > %b | correct output", val_B, ALU_out); 
+    else begin
+      $error("[FAIL] ~%b > %b", val_B, ALU_out);
+      nerr = 1'b1;
+    end
+    assert(Z === 1'b1) $display("[PASS] Z = 1 | correct Z"); 
+    else begin
+      $error("[FAIL] Z = 0 | incorrect Z");
+      nerr = 1'b1;
+    end
+
+    $stop;
+  end
+endmodule: tb_ALU
---
+++ lab5/lab5-deliverables/tb_datapath.sv
@@ -0,0 +1,192 @@
+module tb_datapath(output err);
+    reg nerr = 1'b0;
+    assign err = nerr;
+
+    reg clk, wb_sel, w_en, en_A, en_B, sel_A, sel_B, en_C, en_status;
+    reg [1:0] shift_op, ALU_op;
+    reg [2:0] w_addr, r_addr;
+    reg [15:0] datapath_in;
+
+    wire Z_out;
+    wire [15:0] datapath_out;
+
+    datapath dut(.clk(clk), .datapath_in(datapath_in), .wb_sel(wb_sel), 
+                 .w_addr(w_addr), .w_en(w_en), .r_addr(r_addr), .en_A(en_A),
+                 .en_B(en_B), .shift_op(shift_op), .sel_A(sel_A), .sel_B(sel_B),
+                 .ALU_op(ALU_op), .en_C(en_C), .en_status(en_status),
+                 .datapath_out(datapath_out), .Z_out(Z_out));
+
+    initial begin
+        clk <= 1'b1;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        // fill regfile with numbers
+        // r0 = 6969, r1 = -6969, r2 = 1024, r3 = -1024, 
+        // r4 = 0, r5 = 15, r6 = -15, r7 = X
+        wb_sel = 1'b1;
+        w_en = 1'b1;
+
+        datapath_in = 16'd6969; 
+        w_addr = 3'd0;
+        #20;
+        datapath_in = -16'd6969;
+        w_addr = 3'd1;
+        #20;
+        datapath_in = 16'd1024;
+        w_addr = 3'd2;
+        #20;
+        datapath_in = -16'd1024;
+        w_addr = 3'd3;
+        #20;
+        datapath_in = 16'd0;
+        w_addr = 3'd4;
+        #20;
+        datapath_in = 16'd15;
+        w_addr = 3'd5;
+        #20;
+        datapath_in = -16'd15;
+        w_addr = 3'd6;
+        #20;
+
+        wb_sel = 1'b0;
+        w_en = 1'b0; // prevent garbage data from being written
+
+        // write 6969 to A and B
+        r_addr = 3'd0;
+        en_A = 1'b1;
+        en_B = 1'b1;
+        #20;
+
+        en_A = 1'b0;
+        en_B = 1'b0;
+
+        r_addr = 3'd2; // this should not update A or B
+
+        shift_op = 2'b00; // don't shift
+
+        sel_A = 1'b0;
+        sel_B = 1'b0;
+
+        ALU_op = 2'b00; // ADD
+
+        en_status = 1'b1;
+        en_C = 1'b1;
+        #20; //add more time for netlist to update
+        
+        assert (datapath_out === 16'd13938) $display("[PASS] r0 + r0 = 13938; r0 = 6969; en_C true");
+        else begin
+            $error("[FAIL] r0 + r0 = 13938; r0 = 6969; en_C true");
+            nerr = 1'b1;
+        end
+
+        assert (Z_out === 1'b0) $display("[PASS] Z_out is zero when alu output is 13938 and en_status is true");
+        else begin
+            $error("[FAIL] Z_out is zero when alu output is 13938 and en_status is true");
+            nerr = 1'b1;
+        end
+
+        wb_sel = 1'b0; //write output to r7
+        w_addr = 3'd7;
+        w_en = 1'b1;
+        #20;
+
+        w_en = 1'b0;
+
+        ALU_op = 2'b01; // SUBTRACT
+
+        en_status = 1'b0;
+        en_C = 1'b0;
+        #20;
+
+        assert (datapath_out === 16'd13938) $display("[PASS] en_C false, retaining last output");
+        else begin
+            $error("[FAIL] en_C false, retaining last output");
+            nerr = 1'b1;
+        end
+
+        assert (Z_out === 1'b0) $display("[PASS] en_status false, retaining last output");
+        else begin
+            $error("[FAIL] en_status false, retaining last output");
+            nerr = 1'b1;
+        end
+
+        en_status = 1'b1;
+        en_C = 1'b1;
+        #20; //add more time for netlist to update
+
+        assert (datapath_out === 16'd0) $display("[PASS] r0 - r0 = 0; r0 = 6969; en_C is true");
+        else begin
+            $error("[FAIL] r0 - r0 = 0; r0 = 6969; en_C is true");
+            nerr = 1'b1;
+        end
+
+        assert (Z_out === 1'b1) $display("[PASS] Z_out is one when alu output is 0 and en_status is true");
+        else begin
+            $error("[FAIL] Z_out is one when alu output is 0 and en_status is true");
+            nerr = 1'b1;
+        end
+
+        sel_A = 1'b1;
+        sel_B = 1'b1;
+
+        ALU_op = 2'b00;
+
+        datapath_in[4:0] = 5'd31;
+
+        en_C = 1'b1;
+        #20;
+
+        assert (datapath_out === 16'd31) $display("[PASS] datapath_out equals immediate value of 31, sel_A and sel_B working");
+        else begin
+            $error("[FAIL] datapath_out equals immediate value of 31, sel_A and sel_B working");
+            nerr = 1'b1;
+        end
+
+        r_addr = 3'd7; // write r7 into register B
+
+        en_B = 1'b1; 
+        sel_B = 1'b0;
+        #30;
+
+        assert (datapath_out === 16'd13938) $display("[PASS] datapath_out equals r7 value of 13938");
+        else begin
+            $error("[FAIL] datapath_out equals r7 value of 13938");
+            nerr = 1'b1;
+        end
+
+        shift_op = 2'b01;
+        r_addr = 3'd6;
+        #30;
+
+        assert (datapath_out === -16'd30) $display("[PASS] datapath_out equals r6 * 2 = -30 (left shift one bit)");
+        else begin
+            $error("[FAIL] datapath_out equals r6 * 2 = -30 (left shift one bit)");
+            nerr = 1'b1;
+        end
+
+        sel_B = 1'b1;
+        datapath_in[4:0] = 5'd10;
+        #20;
+
+        assert (datapath_out === 16'd10) $display("[PASS] datapath_out equals immediate value 10, should be unaffected by shifter");
+        else begin
+            $error("[FAIL] datapath_out equals immediate value 10, should be unaffected by shifter");
+            nerr = 1'b1;
+        end
+
+        sel_B = 1'b0;
+        shift_op = 2'b11;
+        #20;
+
+        assert (datapath_out === -16'd8) $display("[PASS] datapath_out equals r6 >>> 1 = (-8)");
+        else begin
+            $error("[FAIL]  datapath_out equals r7 >>> 1 = (-8)");
+            nerr = 1'b1;
+        end
+
+        $stop;
+    end
+
+endmodule: tb_datapath
