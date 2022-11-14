a2421788d6e99146924d68698e660a888c9ea946
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-14 08:45:38 -0800

    e

--- lab6/lab6-files/datapath/datapath.sv
+++ lab6/lab6-files/datapath/datapath.sv
@@ -2,7 +2,7 @@ module datapath(input clk, input [15:0] mdata, input [7:0] pc, input [1:0] wb_se
                 input [2:0] w_addr, input w_en, input [2:0] r_addr, input en_A,
                 input en_B, input [1:0] shift_op, input sel_A, input sel_B,
                 input [1:0] ALU_op, input en_C, input en_status,
-		input [15:0] sximm8, input [15:0] sximm5,
+		            input [15:0] sximm8, input [15:0] sximm5,
                 output [15:0] datapath_out, output Z_out, output N_out, output V_out);
   //for regfile
   reg [15:0] w_data;
