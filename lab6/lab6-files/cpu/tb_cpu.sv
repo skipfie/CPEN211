9eae0bfb74c080a95f0a94a0604e5f04787ddd0a
Joe <skipfie@gmail.com>
2022-11-15 15:27:40 -0800

    integer

--- lab6/lab6-files/cpu/tb_cpu.sv
+++ lab6/lab6-files/cpu/tb_cpu.sv
@@ -1,3 +1,41 @@
 module tb_cpu(output err);
-  // your implementation here
+    // tb vars
+    reg nerr = 1'b0;
+    assign err = nerr;
+
+    integer failed = 0;
+    integer passed = 0;
+
+    // cpu i/o
+    reg clk, rst_n, load, start;
+    reg [15:0] instr;
+
+    wire waiting, N, V, Z;
+    wire [15:0] out
+
+    cpu dut(.clk(clk), .rst_n(rst_n), .load(load), .start(start), .instr(instr),
+            .waiting(waiting), .out(out), .N(N), .V(V), .Z(Z));
+
+    task reset; rst_n = 1'b0; #20; rst_n = 1'b1; #10; endtask
+
+    task ADD();
+
+    endtask
+
+    initial begin
+        clk <= 1'b1;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+
+    end
+
+    initial begin
+        reset;
+
+
+        $stop;
+    end
+
 endmodule: tb_cpu
