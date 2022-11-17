cdd3370ab01b9258e89fa5c6afac8ab58c316605
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-17 12:19:07 -0800

    Update tb_controller.sv

--- lab6/lab6-files/controller/tb_controller.sv
+++ lab6/lab6-files/controller/tb_controller.sv
@@ -8,7 +8,7 @@ module tb_controller(output err);
   reg [1:0] ALU_op, shift_op;
   reg [2:0] opcode;
 
-  //controller input
+  //controller output
   wire waiting, en_A, en_B, en_C, en_status, sel_A, sel_B, w_en;
   wire [1:0] reg_sel, wb_sel; 
 
