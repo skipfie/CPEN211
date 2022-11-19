a4ede42fe0dc41988a6878fe5fef301947c71160
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-19 14:03:49 -0800

    🥲

--- lab6/lab6-files/controller/controller2/controller2.sv
+++ lab6/lab6-files/controller/controller2/controller2.sv
@@ -10,7 +10,6 @@ module controller2(input clk, input rst_n, input start,
 
     reg _waiting, _en_A, _en_B, _en_C, _en_status, _sel_A, _sel_B, _w_en;
     reg [1:0] _reg_sel, _wb_sel; 
-    reg [4:0] state;
     assign waiting = _waiting; //if the ctl is busy computing, waiting is 0, otherwise waiting is 1
     assign en_A = _en_A;
     assign en_B = _en_B;
@@ -23,80 +22,97 @@ module controller2(input clk, input rst_n, input start,
     assign wb_sel = _wb_sel;
     
     // define the name of each state
-    `define Wait 5'd0
-
-    `define mov1 5'd1
-
-    `define mov_1 5'd2
-    `define mov_2 5'd3
-    `define mov_3 5'd4
-
-    `define mvn1 5'd5
-    `define mvn2 5'd6
-    `define mvn3 5'd7
-
-    `define add1 5'd8
-    `define add2 5'd9
-    `define add3 5'd10
-    `define add4 5'd11
-
-    `define cmp1 5'd12
-    `define cmp2 5'd13
-    `define cmp3 5'd14
-    `define cmp4 5'd15
-
-    `define And1 5'd16
-    `define And2 5'd17
-    `define And3 5'd18
-    `define And4 5'd19
+    enum reg [4:0] {Wait, mov1, mov_1, mov_2, mov_3, mvn1, mvn2, mvn3, 
+                    add1, add2, add3, add4, cmp1, cmp2, cmp3, cmp4, and1, and2, and3, and4} state;
 
     // for state transition
     always_ff @(posedge clk) begin
-        if (~rst_n) state <= `Wait; //active low reset
+        if (~rst_n) state <= Wait; //active low reset
         else begin 
             case (state)
-            `Wait: begin
-                if (start && opcode == 3'b101 && ALU_op == 2'b00) state <= `add1;
-                else if (start && opcode == 3'b101 && ALU_op == 2'b01) state <= `cmp1;
-                else if (start && opcode == 3'b101 && ALU_op == 2'b10) state <= `And1;
-                else if (start && opcode == 3'b101 && ALU_op == 2'b11) state <= `mvn1;
+            Wait: begin
+                if (start && opcode == 3'b101 && ALU_op == 2'b00) state <= add1;
+                else if (start && opcode == 3'b101 && ALU_op == 2'b01) state <= cmp1;
+                else if (start && opcode == 3'b101 && ALU_op == 2'b10) state <= and1;
+                else if (start && opcode == 3'b101 && ALU_op == 2'b11) state <= mvn1;
 
-                else if (start && opcode == 3'b110 && ALU_op == 2'b10) state <= `mov1;
-                else if (start && opcode == 3'b110 && ALU_op == 2'b00) state <= `mov_1;
-                else state <= `Wait;
+                else if (start && opcode == 3'b110 && ALU_op == 2'b10) state <= mov1;
+                else if (start && opcode == 3'b110 && ALU_op == 2'b00) state <= mov_1;
+                else state <= Wait;
             end 
-            `mov1: state <= `Wait;
-            `mov_1: state <= `mov_2;
-            `mov_2: state <= `mov_3;
-            `mov_3: state <= `Wait;
+            mov1: state <= Wait;
+            mov_1: state <= mov_2;
+            mov_2: state <= mov_3;
+            mov_3: state <= Wait;
 
-            `add1: state <= `add2;
-            `add2: state <= `add3;
-            `add3: state <= `add4;
-            `add4: state <= `Wait;
+            add1: state <= add2;
+            add2: state <= add3;
+            add3: state <= add4;
+            add4: state <= Wait;
 
-            `cmp1: state <= `cmp2;
-            `cmp2: state <= `cmp3;
-            `cmp3: state <= `cmp4;
-            `cmp4: state <= `Wait;
+            cmp1: state <= cmp2;
+            cmp2: state <= cmp3;
+            cmp3: state <= cmp4;
+            cmp4: state <= Wait;
 
-            `And1: state <= `And2;
-            `And2: state <= `And3;
-            `And3: state <= `And4;
-            `And4: state <= `Wait;
+            and1: state <= and2;
+            and2: state <= and3;
+            and3: state <= and4;
+            and4: state <= Wait;
 
-            `mvn1: state <= `mvn2;
-            `mvn2: state <= `mvn3;
-            `mvn3: state <= `Wait;
-            default: state <= `Wait;
+            mvn1: state <= mvn2;
+            mvn2: state <= mvn3;
+            mvn3: state <= Wait;
+            default: state <= Wait;
             endcase
         end
     end
 
     // for output logic
-    always_comb begin
+    assign _waiting = (state == Wait) ? 1'b1 : 1'b0 ;
+
+    assign _en_A = (state == add1) ? 1'b1 :
+                   (state == cmp1) ? 1'b1 :
+                   (state == and1) ? 1'b1 : 1'b0 ;
+
+    assign _en_B = (state == add2) ? 1'b1 :
+                   (state == cmp2) ? 1'b1 :
+                   (state == and2) ? 1'b1 :
+                   (state == mvn1) ? 1'b1 :
+                   (state == mov_1) ? 1'b1 : 1'b0 ;
+
+    assign _en_C = (state == add3) ? 1'b1 :
+                   (state == and3) ? 1'b1 :
+                   (state == mvn2) ? 1'b1 :
+                   (state == mov_2) ? 1'b1 : 1'b0 ;
+
+    assign _en_status = (state == cmp4) ? 1'b1 : 1'b0 ;
+
+    assign _sel_A = (state == mvn2) ? 1'b1 :
+                    (state == mov_2) ? 1'b1 : 1'b0 ;
+
+    assign _sel_B = 1'b0;
+
+    assign _w_en = (state == add4) ? 1'b1 :
+                   (state == mvn3) ? 1'b1 :
+                   (state == and4) ? 1'b1 :
+                   (state == mov1) ? 1'b1 :
+                   (state == mov_3) ? 1'b1 : 1'b0 ;
+    
+    assign _reg_sel = (state == add1) ? 2'b10 :
+                      (state == cmp1) ? 2'b10 :
+                      (state == and1) ? 2'b10 :
+                      (state == mov1) ? 2'b10 :
+                      (state == add4) ? 2'b01 :
+                      (state == and4) ? 2'b01 :
+                      (state == mvn3) ? 2'b01 :
+                      (state == mov_3) ? 2'b01 : 2'b00 ;
+    
+    assign _wb_sel = (state == mov1) ? 2'b10 : 2'b00 ;
+
+   /*always_comb begin
         case (state)
-            `Wait: begin 
+            Wait: begin 
                 _waiting = 1'b1;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -110,7 +126,7 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b11;
             end
             
-            `add1: begin
+            add1: begin
                 _waiting = 1'b0;
                 _en_A = 1'b1;
                 _en_B = 1'b0;
@@ -123,7 +139,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end 
-            `add2: begin
+            add2: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b1;
@@ -136,7 +152,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b11;
             end 
-            `add3: begin
+            add3: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -149,7 +165,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end 
-            `add4: begin
+            add4: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -163,7 +179,7 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b11;
             end 
 
-            `cmp1: begin
+            cmp1: begin
                 _waiting = 1'b0;
                 _en_A = 1'b1;
                 _en_B = 1'b0;
@@ -176,7 +192,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end
-            `cmp2: begin
+            cmp2: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b1;
@@ -189,7 +205,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b11;
             end
-            `cmp3: begin
+            cmp3: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -202,7 +218,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end
-            `cmp4: begin
+            cmp4: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -216,7 +232,7 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b11;
             end
 
-            `And1: begin
+            and1: begin
                 _waiting = 1'b0;
                 _en_A = 1'b1;
                 _en_B = 1'b0;
@@ -229,7 +245,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end 
-            `And2: begin
+            and2: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b1;
@@ -242,7 +258,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b11;
             end 
-            `And3: begin
+            and3: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -255,7 +271,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end 
-            `And4: begin
+            and4: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -269,7 +285,7 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b11;
             end 
 
-            `mvn1: begin
+            mvn1: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b1;
@@ -282,7 +298,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end
-            `mvn2: begin
+            mvn2: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -295,7 +311,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b11;
             end
-            `mvn3: begin
+            mvn3: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -309,7 +325,7 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b100;
             end
 
-            `mov1: begin
+            mov1: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -323,7 +339,7 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b11;
             end
 
-            `mov_1: begin
+            mov_1: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b1;
@@ -336,7 +352,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b00;
             end
-            `mov_2: begin
+            mov_2: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -349,7 +365,7 @@ module controller2(input clk, input rst_n, input start,
                 _wb_sel = 2'b00;
                 test_port1 = 2'b11;
             end
-            `mov_3: begin
+            mov_3: begin
                 _waiting = 1'b0;
                 _en_A = 1'b0;
                 _en_B = 1'b0;
@@ -376,5 +392,5 @@ module controller2(input clk, input rst_n, input start,
                 test_port1 = 2'b11;
             end
         endcase
-    end
+    end*/
 endmodule: controller2
