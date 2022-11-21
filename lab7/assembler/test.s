495a3f57fe83ab3c01ab018c027e6410e8375ff8
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-20 21:18:18 -0800

    added lab 7

---
+++ lab7/assembler/test.s
@@ -0,0 +1,3 @@
+MOV R0, #7 ; this means, take the absolute number 7 and store it in R0
+MOV R1, #2 ; this means, take the absolute number 2 and store it in R1
+ADD R2, R1, R0, LSL#1 ; this means R2 = R1 + (R0 shifted left by 1) = 2+14=16
