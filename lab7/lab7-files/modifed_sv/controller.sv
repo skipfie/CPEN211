3c6bb000cbb16672d81a2108a49bb9de4a7ddde8
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-23 22:11:37 -0800

    Update controller.sv

--- lab7/lab7-files/modifed_sv/controller.sv
+++ lab7/lab7-files/modifed_sv/controller.sv
@@ -102,7 +102,7 @@ module controller(input clk, input rst_n, input [2:0] opcode, input [1:0] ALU_op
 
     assign clear_pc = (state == fd&rs) ? 1'b1 : 1'b0;
 
-    assign ram_w_en = 
+    assign ram_w_en = (state)
 
     assign sel_addr = 
 
