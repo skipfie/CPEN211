c76dee772a1c9f18934bd1353ab6122335b3f5f6
Joe <skipfie@gmail.com>
2022-11-23 19:37:47 -0800

    so not epic marcus :(

--- lab7/lab7-files/task1/task1.sv
+++ lab7/lab7-files/task1/task1.sv
@@ -1,3 +1,8 @@
-module task1(input clk, input rst_n, input [7:0] start_pc, output[15:0] out);
-  // your implementation here
+module task1(input clk, input rst_n, input [7:0] start_pc, output [15:0] out);
+    // to ram
+    wire [7:0] ram_addr;
+
+    ram ram(.clk(clk), .ram_w_en(ram_w_en), 
+            .ram_r_addr(ram_addr), .ram_w_addr(ram_addr), 
+            .ram_w_data(ram_w_data), .ram_r_data(ram_r_data));
 endmodule: task1
