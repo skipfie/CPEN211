ce99ed0747e6e07fb2247740e8a0435a41dede5a
Joe <skipfie@gmail.com>
2022-11-23 22:05:39 -0800

    ms paint art

--- lab7/lab7-files/task2/task2.sv
+++ lab7/lab7-files/task2/task2.sv
@@ -1,3 +1,8 @@
 module task2(input clk, input rst_n, input [7:0] start_pc, output[15:0] out);
-  // your implementation here
+    // how to do ldr
+    /*
+    ldr 1:
+    
+
+    */
 endmodule: task2
