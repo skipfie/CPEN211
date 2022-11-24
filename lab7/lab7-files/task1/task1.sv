363d4c09056d1c87080ead5dc24ab406ea7f2b6d
Marcus <75653877+Beluguy@users.noreply.github.com>
2022-11-24 12:06:43 -0800

    e

--- lab7/lab7-files/task1/task1.sv
+++ lab7/lab7-files/task1/task1.sv
@@ -317,21 +317,20 @@ module controller(input clk, input rst_n, input [2:0] opcode, input [1:0] ALU_op
 
     assign load_ir = (state == fdr) ? 1'b1 :
                      (state == fd) ? 1'b1 :
-                     (state == add2) ? 1'b1 : 
-                     (state == cmp2) ? 1'b1 :
-                     (state == and2) ? 1'b1 :
-                     (state == mvn2) ? 1'b1 :
-                     (state == mov1) ? 1'b1 :
-                     (state == mov_2) ? 1'b1 : 1'b0; 
-
-    assign load_pc = (state == fdr) ? 1'b1 :
-                     //(state == fd) ? 1'b1 :
                      (state == add4) ? 1'b1 : 
-                     (state == cmp3) ? 1'b1 :
+                     (state == cmp4) ? 1'b1 :
                      (state == and4) ? 1'b1 :
                      (state == mvn3) ? 1'b1 :
                      (state == mov1) ? 1'b1 :
                      (state == mov_3) ? 1'b1 : 1'b0; 
+
+    assign load_pc = (state == fdr) ? 1'b1 :
+                     (state == add2) ? 1'b1 : 
+                     (state == cmp1) ? 1'b1 :
+                     (state == and2) ? 1'b1 :
+                     (state == mvn1) ? 1'b1 :
+                     (state == mov1) ? 1'b1 :
+                     (state == mov_1) ? 1'b1 : 1'b0; 
 
     assign clear_pc = (state == fdr) ? 1'b1 : 1'b0;
 
